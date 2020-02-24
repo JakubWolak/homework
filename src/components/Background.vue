fd13070228df1337942a9f10c1f1831362d3768f
JakubWolak <jakubwolak.123@gmail.com>
2020-02-24 22:31:54 +0100

    add background component

---
+++ src/components/Background.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="image" />
+</template>
+
+<script>
+export default {
+  name: 'Background',
+};
+</script>
+
+<style lang="scss" scoped>
+.image{
+  position: fixed;
+  top: 0;
+  z-index: -1;
+  width: 100%;
+  min-height: 100vh;
+  background-image: url('../assets/background.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 60% 0;
+}
+</style>
